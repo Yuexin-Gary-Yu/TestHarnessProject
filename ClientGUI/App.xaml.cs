a7cdead9b410a1f7d97ae529d15383f673781ed4
Yuexin Yu <yyu100@syr.edu>
2020-05-29 01:33:34 -0700

    OOD-Project

---
+++ ClientGUI/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *-------------------------------------
+ * Yuexin Yu
+ * CSE 687 Object Oriented Design C++
+ * Syracuse University
+ * Design Project: Test Harness
+ *-------------------------------------
+ * File Name: App.xaml.cs
+ * Project Name: GUI
+ * Required Files:
+ * Last Modified: Monday, 3rd December 2018 5:04:06 pm
+ * Modified By:
+ * ----------
+ */
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+using MsgPassingCommunication;
+
+namespace GUI
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+
+    public App()
+    {
+            
+    }
+   
+  }
+}
